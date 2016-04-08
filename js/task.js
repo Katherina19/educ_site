e83e49c3f0c4a655f0619cc37823a06cd642f64a
Katherine <182228@niuitmo.ru>
2016-04-08 08:19:51 +0300

    Добавлены новые файлы, придуманы задания.

--- js/task.js
+++ js/task.js
@@ -2,5 +2,5 @@
  * Created by Katherine on 04.04.2016.
  */
 var editor = ace.edit("editor");
-editor.setTheme("ace/theme/monokai");
+editor.setTheme("ace/theme/clouds");
 editor.getSession().setMode("ace/mode/javascript");
